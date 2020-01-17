8659a77fccd6f8721d7e936e4b1d15806cad9f53
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2020-01-17 18:20:34 +0100

    [ FIX ] Layout print crontabs
    [ FIX ] Layout print crontabs

--- apps/_appsgen.sh
+++ apps/_appsgen.sh
@@ -29,5 +29,7 @@ sed -i "/^_templates.yml\b/Id" /var/plexguide/app.list
 sed -i -e "/oauth/d" /var/plexguide/app.list
 sed -i -e "/dockergc/d" /var/plexguide/app.list
 sed -i -e "/pgui/d" /var/plexguide/app.list
+sed -i -e "/oauth/d" /var/plexguide/app.list
+sed -i -e "/dockergc/d" /var/plexguide/app.list
 sed -i -e "/crontabs/d" /var/plexguide/app.list
 sed -i -e "/archive/d" /var/plexguide/app.list
